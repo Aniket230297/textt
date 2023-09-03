37bdcd3a2e5532c7a1aaac862389eaaf0a7876f7
Aniket <aniketkule231997@gmail.com>
2023-09-03 22:25:26 +0530

    update

---
+++ src/Components/Search.js
@@ -0,0 +1,13 @@
+
+import "./Search.css"
+
+const Search = ()=>{
+    return(
+        <div>
+            <input  type="search" />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Search
